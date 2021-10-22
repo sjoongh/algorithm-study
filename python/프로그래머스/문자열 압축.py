5ea004dcd3473de4cea5934ce0771fab42ccd5a9
sjoongh <az45687@naver.com>
2021-10-22 21:52:04 +0900

    문자열 압축 수정

--- python/프로그래머스/문자열 압축.py
+++ python/프로그래머스/문자열 압축.py
@@ -1,41 +1,34 @@
 def solution(s):
-    result = []
-    if len(s) == 1:
-        return 1
-    for i in range(1, (len(s) // 2)+1):
-        b = ''
-        count = 1
-        tmp = s[:i]
-        
+    # 최소길이
+    answer = len(s)
+
+    # 자르는 단위 늘려가며 최소 길이 계산
+    for i in range(1, len(s) //2+1):
+        result = "" # 압축된 문자열
+        count = 1 # 반복 횟수
+        compare = s[0:i] # 자른 문자열
+
+        # 압축된 문자열 구하기
         for j in range(i, len(s), i):
-            #
-            if tmp == s[j:i+j]:
+            # 문자열이 반복 되는 경우
+            if compare == s[j:j+i]:
                 count += 1
-print(solution("ababcdcdababcdcd"))
-
-
-
-def solution(s):
-    result=[]
-    if len(s)==1:
-        return 1
-    for i in range(1, (len(s)//2)+1):
-        b = ''
-        cnt = 1
-        tmp=s[:i]
-        for j in range(i, len(s), i):
-            if tmp==s[j:i+j]:
-                cnt+=1 
+            # 더 이상 압축할 수 없을 경우
             else:
-                if cnt!=1:
-                    b = b + str(cnt)+tmp
+                if count == 1:
+                    result += compare
+                    compare = s[j:j+i]
                 else:
-                    b = b + tmp
-                tmp=s[j:j+i]
-                cnt = 1
-            if cnt!=1:
-                b = b + str(cnt) + tmp
-            else:
-                b = b + tmp
-            result.append(len(b))
-        return min(result)
+                    result += (str(count) + compare)
+                    count = 1
+                    compare = s[j:j+i]
+        # 나머지 문자열 처리
+        if count == 1:
+            result += compare
+        else:
+            result += (str(count) + compare)
+
+        # 최소 길이 계산
+        answer = min(answer, len(result))
+
+    return answer
