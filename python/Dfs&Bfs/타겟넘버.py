daee0041a88994c93f8c6105e6b53c3c4b93dbf6
sjoongh <az45687@naver.com>
2021-10-24 20:15:32 +0900

    타겟넘버

---
+++ python/Dfs&Bfs/타겟넘버.py
@@ -0,0 +1,18 @@
+# bfs 방식
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        tmp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([tmp+numbers[idx], idx])
+            queue.append([tmp-numbers[idx], idx])
+        else:
+            if tmp == target:
+                answer += 1
+    return answer
