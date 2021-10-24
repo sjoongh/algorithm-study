9bbf7f257eb488bc190aef32f31f77738515f9a8
sjoongh <az45687@naver.com>
2021-10-24 21:09:21 +0900

    타겟넘버 dfs

--- python/Dfs&Bfs/타겟넘버.py
+++ python/Dfs&Bfs/타겟넘버.py
@@ -16,3 +16,18 @@ def solution(numbers, target):
             if tmp == target:
                 answer += 1
     return answer
+
+# dfs방식
+def solution(numbers, target):
+    # tree는 이전수에 대한 계산 결과를 담은 층
+    tree = [0]
+    # 숫자 목록을 하나씩 꺼내와 주는 역할
+    for num in numbers:
+        sub_tree = []
+        # 노드 하나하나에 숫자를 더하고 빼주어 자식 노드를 생성
+        for tree_num in tree:
+            sub_tree.append(tree_num + num)
+            sub_tree.append(tree_num - num)
+        # sub_tree는 현재 숫자에 대한 결과를 담은 자식 노드를 하나씩 추가
+        tree = sub_tree
+    return tree.count(target)
