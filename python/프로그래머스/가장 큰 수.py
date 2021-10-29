838cc62407186d7101d9838d1df8355f2c0aa33e
sjoongh <az45687@naver.com>
2021-10-29 19:02:44 +0900

    10-29 가장 큰 수

--- python/프로그래머스/가장 큰 수.py
+++ python/프로그래머스/가장 큰 수.py
@@ -1,8 +1,4 @@
 def solution(numbers):
-    answer = ''
-    test = ''
-    # 조건 1. 앞자리 num이 높은게 출력
-    # 조건 2. 9가 쓰이려면 99, 98
-    # 방법1. numbers를 하나씩 쪼개서 계산
-    for i in numbers:
-        test += str(i)
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    return str(int(''.join(numbers)))
