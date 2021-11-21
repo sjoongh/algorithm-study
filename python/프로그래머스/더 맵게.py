298f903a9352ab11b15a8c842169db5337c94794
sjoongh <az45687@naver.com>
2021-11-21 19:32:15 +0900

    더맵게 효율성부분 고치는중

--- python/프로그래머스/더 맵게.py
+++ python/프로그래머스/더 맵게.py
@@ -1,14 +1,13 @@
 def solution(scoville, K):
-    # 가장 low 숫자 + (두번째로 low한 숫자 * 2)
-    # 반복문의 반복은 K이상이 될때까지 섞는다
     answer = 0
-    ans_li = []
-    scoville.sort()
-    for i in range(len(scoville)):
-        if scoville[i] < K:
-            ans_li.append(scoville[i])
-    for j in range(len(scoville)):
-        if scoville[j] < K and scoville[j+1] < K:
-            min_num = scoville.popleft()
-            min_num2 = scoville.popleft()
+    while min(scoville) < K:
+        scoville.sort()
+        # 에러 처리
+        try:
+            if scoville[0] < K:
+                scoville.insert(0, scoville.pop(0)+(scoville.pop(0) * 2))
+        # 모든 음식을 k이상으로 만들 수 없는 경우 -1을 return
+        except IndexError:
+            return -1
+        answer += 1
     return answer
