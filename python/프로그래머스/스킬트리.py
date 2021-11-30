9923fa658306ef0b22f7677a27b455f7e164e913
sjoongh <az45687@naver.com>
2021-11-30 21:30:20 +0900

    스킬트리

---
+++ python/프로그래머스/스킬트리.py
@@ -0,0 +1,15 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for i in skill_trees:
+        skill_li = list(skill)
+        for j in i:
+            # BACDE
+            if j in skill:
+                # skill_li의 순서대로 진행되어야 하므로
+                # 앞글자부터 pop으로 뺄 때 순서가 맞지 않는다면
+                # 틀렸으므로 break, 아닐경우는 answer += 1
+                if j != skill_li.pop(0):
+                    break
+            else:
+                answer += 1
+    return answer
