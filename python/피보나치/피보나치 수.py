5867792617e34b74ec29438e10ea2d82ca73c9a8
sjoongh <az45687@naver.com>
2021-12-01 20:42:35 +0900

    피보나치 방법3

--- python/피보나치/피보나치 수.py
+++ python/피보나치/피보나치 수.py
@@ -1,3 +1,4 @@
+# 방법1.
 def solution(n):
     answer = 0
     a,b = 0,1
@@ -10,6 +11,7 @@ def solution(n):
     return answer
 
 
+# 방법 2. 
 # 재귀함수를 사용한 피보나치, 시간효율이 안 좋음
 # def F(n):
 #     if n < 2:
@@ -22,4 +24,18 @@ def solution(n):
 #     for i in range(n):
 #         answer = F(n)
 #     answer %= 1234567
-#     return answer    
+#     return answer
+
+
+# 방법 3.
+# 방법1보다는 직관적 방법
+# def solution(n):
+#     answer = []
+#     for i in range(n+1):
+#         if i==0 or i==1:
+#             answer.append(i)
+#         else:
+#             f = answer[i-1] + answer[i-2]
+#             answer.append(f % 1234567)
+
+#     return answer[-1]
