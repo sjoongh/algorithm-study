4759f4b1802f0ce948902a7bd6332a5ac220f16f
sjoongh <az45687@naver.com>
2021-12-09 10:50:49 +0900

    1

--- python/프로그래머스/오픈채팅방.py
+++ python/프로그래머스/오픈채팅방.py
@@ -1,14 +1,17 @@
 def solution(record):
     answer = []
-    nickname = {}
+    nickname = {} # 딕셔너리
     
     for r in record:
         row = r.split(' ')
         if row[0] == 'Enter' or row[0] == 'Change':
+            # nickname 딕셔너리 { row[1]:key = row[2]:value } 형태로 삽입됨
+            # 딕셔너리에 key값을 추가할때는 nickname[key] = value 처럼 사용
             nickname[row[1]] = row[2]  
     for r in record:
         row = r.split(' ')
         if row[0] == 'Enter':
+            # nickname 딕셔너리에 row[1]에 해당하는 value + 'String'
             answer.append(nickname[row[1]] + '님이 들어왔습니다.')
         elif row[0] == 'Leave':
             answer.append(nickname[row[1]] + '님이 나갔습니다.')
