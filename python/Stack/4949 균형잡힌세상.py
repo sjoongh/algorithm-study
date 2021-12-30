b117cbee8b444c06b5ce0425141afa744ddf23b7
sjoongh <az45687@naver.com>
2021-12-30 18:17:10 +0900

    fix 4949

--- python/Stack/4949 균형잡힌세상.py
+++ python/Stack/4949 균형잡힌세상.py
@@ -1,21 +1,21 @@
 while(True):
     li = []
     s = input()
-    if s == '.':
+    # 힌트 : 7번째의 " ."와 같이 괄호가 하나도 없는 경우도 균형잡힌 문자열로 간주할 수 있다.
+    if s == '.': # 때문에 'yes'라고 따로 출력문을 선언하지 않아도 무관, .이 나오면 반복문 종료 
         break
     for i in s:
-        if i == '(' or i == '[':
-            li.append(i)
-
-        elif i == ')':
-            if len(li) == 0:
+        if i == '(' or i == '[': # 시작 괄호가 존재하면
+            li.append(i) # 넣어줌
+        elif i == ')': # 닫는괄호가 존재하면
+            if len(li) == 0: # ???
                 li.append('.')
                 break
-            elif li[-1] != '(':
+            elif li[-1] != '(': # 올바른 문자열인지 확인
                 break
             else:
-                li.pop()
-        elif i == ']':
+                li.pop() # 올바른 문자열이므로 제외
+        elif i == ']': # 닫는괄호가 존재하면
             if len(li) == 0:
                 li.append('.')
                 break
