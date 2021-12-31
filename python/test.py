207d50ec6e09b21c9862d17f13b14095f2292d99
sjoongh <az45687@naver.com>
2021-12-31 15:41:39 +0900

    test

--- python/test.py
+++ python/test.py
@@ -177,18 +177,3 @@
 #         print(dict1[i], end=' ')
 #     else:
 #         print(0, end=' ')
-
-
-# 백준 15829
-L = int(input())
-M = 1234567891
-r = 31
-user_input = input()
- 
-answer = 0
- 
-for i in range(len(user_input)):
-    num = ord(user_input[i]) - 96
-    answer += num * (r ** i)
- 
-print(answer % M)
