7268f37e6cf0e16f160002c4717f202114f016cd
sjoongh <az45687@naver.com>
2022-01-31 22:27:51 +0900

    x보다 작은수-10871

--- Java/백준 10871.java
+++ Java/백준 10871.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int x = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            int number = sc.nextInt();
+            if (number < x) {
+                System.out.print(number+" ");
+            }
+        }
     }
 }
