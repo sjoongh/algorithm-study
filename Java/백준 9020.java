076498806f232ec1927ef0e47d4c980702aced7d
sjoongh <az45687@naver.com>
2022-02-11 21:35:33 +0900

    fix

--- Java/백준 9020.java
+++ Java/백준 9020.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class 백준 9020 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int test = 0;
+        for (int i = 0; i < t; i++) {
+            test = sc.nextInt();
+        }
     }
 }
