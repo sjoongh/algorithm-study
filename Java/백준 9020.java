96921603e191763d07489c818e06dd5c65f82801
sjoongh <az45687@naver.com>
2022-02-13 20:50:00 +0900

    9020 fix

--- Java/백준 9020.java
+++ Java/백준 9020.java
@@ -12,15 +12,16 @@ public class Main {
 		for (int i = 0; i < T; i++) {
 			int n = sc.nextInt();
 			
+            // 두 수의 차이가 가장 작은 것을 출력해야 하므로 n의 절반값부터 검사
 			int div, rem;
 			div = n / 2;
 			rem = n - div;
 			
 			while (true) {
-				if (isPrime(div) && isPrime(rem)) {
-					break;
+				if (isPrime(div) && isPrime(rem)) { // 두 수가 소수인지 판별
+					break; // 소수라면 break로 while문 나감
 				} else {
-					div--;
+					div--; // 처음 나눈 반이 소수가 아니라면 하나는 낮추고 하나는 높임
 					rem++;
 				}
 			}
@@ -29,9 +30,9 @@ public class Main {
 		}
 	static boolean isPrime(int n) {
 		boolean check = true;
-		for (int i = 2; i <= Math.sqrt(n); i++) {
+		for (int i = 2; i <= Math.sqrt(n); i++) { // n이 2이상에서 나눠 떨어지면 소수가 아니다.
 			if (n % i == 0)
-				check = false;
+				check = false; // 소수면 true 리턴
 		}
 		return check;
 	}
