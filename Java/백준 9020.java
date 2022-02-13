8825e49a2bb4dc6027ad01b319106e887cae7b93
sjoongh <az45687@naver.com>
2022-02-13 21:02:21 +0900

    end 9020

--- Java/백준 9020.java
+++ Java/백준 9020.java
@@ -7,30 +7,34 @@ public class Main {
 		// 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다
 		// 짝수n을 만드는 것 중 두 소수의 차이가 가장 작은 것을 출력!
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int t = sc.nextInt();
 		
-		for (int i = 0; i < T; i++) {
+		for (int i = 0; i < t; i++) {
 			int n = sc.nextInt();
 			
             // 두 수의 차이가 가장 작은 것을 출력해야 하므로 n의 절반값부터 검사
-			int div, rem;
-			div = n / 2;
-			rem = n - div;
+			int up, down; // 16
+			up = n / 2; // 8
+			down = n - up; // 8
 			
 			while (true) {
-				if (isPrime(div) && isPrime(rem)) { // 두 수가 소수인지 판별
-					break; // 소수라면 break로 while문 나감
+                // 두 수 모두 소수여야 하므로 판별
+				if (checking(up) && checking(down)) {
+					break;  
+                    // 소수가 아니라면 소수인 수를 찾기위해 up && down
 				} else {
-					div--; // 처음 나눈 반이 소수가 아니라면 하나는 낮추고 하나는 높임
-					rem++;
+					up--; // 처음 나눈 반이 소수가 아니라면 하나는 낮추고 하나는 높임
+					down++;
 				}
 			}
-			System.out.println(div + " " + rem);
+			System.out.println(up + " " + down);
 		}
 		}
-	static boolean isPrime(int n) {
+	static boolean checking(int n) {
 		boolean check = true;
-		for (int i = 2; i <= Math.sqrt(n); i++) { // n이 2이상에서 나눠 떨어지면 소수가 아니다.
+        // n이 2이상 <= n의제곱근에서 나누어 떨어지면 소수가 아니다.
+        // 1과 자기자신만 존재해야 소수이므로
+		for (int i = 2; i <= Math.sqrt(n); i++) { // Math.sqrt(n) : n의 제곱근을 구함
 			if (n % i == 0)
 				check = false; // 소수면 true 리턴
 		}
