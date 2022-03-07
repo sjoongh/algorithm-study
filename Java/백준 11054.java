2b533da4d94a2269b9dfc210b077c3fb7f765df9
sjoongh <az45687@naver.com>
2022-03-07 21:14:43 +0900

    백준

--- Java/백준 11054.java
+++ Java/백준 11054.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-
+import javafx.scene.chart.ScatterChart;
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+    	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int n = in.nextInt();
+		int n = sc.nextInt();
 		int a[] = new int[n];
 		int l_dp[] = new int[n];
 		int r_dp[] = new int[n];
 		
-		for(int i=0;i<n;i++) {
-			a[i] = in.nextInt();
-		}
+		for(int i=0;i<n;i++) { a[i] = sc.nextInt(); }
 		
-		// LIS
 		for(int i=0;i<n;i++) {
 			l_dp[i] = 1;
 			
@@ -24,8 +20,6 @@ public class Main {
 				}
 			}
 		}
-		
-		// LDS
 		for(int i=n-1;i>=0;i--) {
 			r_dp[i] = 1;
 			
@@ -35,20 +29,13 @@ public class Main {
 				}
 			}
 		}
-		
 		int max = Integer.MIN_VALUE;
-		
-        // LIS와 LDS의 부분수열 길이의 합 중 최댓값을 찾는다.
 		for(int i=0;i<n;i++) {
 			if(max<l_dp[i]+r_dp[i]) {
 				max = l_dp[i] + r_dp[i];
 			}
 		}
-		
-        // 중복 제거를 위한 -1
 		System.out.println(max - 1);
-		
-		in.close();
 	}
 
 }
