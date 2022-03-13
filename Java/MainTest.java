e01f7485a467665c2d1f2b8e43b3fdde40b14979
sjoongh <az45687@naver.com>
2022-03-13 19:59:55 +0900

    find interval

--- Java/MainTest.java
+++ Java/MainTest.java
@@ -1,9 +1,26 @@
 import java.util.*;
 
-public class MainTest {
-       public static void main(String[] args) {
-           String str = "test";
-           int index = str.indexOf("t");
-           System.out.println(index);
-       }
+class Solution {
+    private static String reverseString(String str) {
+        char[] arr = str.toCharArray();
+        for(int i = 0; i < str.length() / 2; i++) {
+            char ch = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = ch;
+        }
+        return new String(arr);
+    }
+    
+    public String solution(String S, int[][] interval) {
+        
+        for(int i = 0; i < interval.length ; i++) {
+            int startIdx = interval[i][0] - 1;
+            int endIdx = interval[i][1];
+            String subString = reverseString(S.substring(startIdx, endIdx));
+            String startString = S.substring(0, startIdx);
+            String endString = S.substring(endIdx);
+            S = startString + subString + endString;
+        }
+        return S;
+    }
 }
