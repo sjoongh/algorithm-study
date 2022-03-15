8fb1fabedcb3abf3bdd1370ed01ad636a411fc65
sjoongh <az45687@naver.com>
2022-03-15 19:57:06 +0900

    2309 detail

--- Java/백준 2309.java
+++ Java/백준 2309.java
@@ -25,8 +25,9 @@ public class Solution {
         for(int i = 0; i < 9 ; i++){
             for(int j = i+1; j < 9 ; j++){
                 int result = sum - dwarf[i] - dwarf[j];
-                if(result == 100){
+                if(result == 100){ // result가 100일 경우
                     for(int k = 0 ; k < 9; k ++){
+                        // dwarf[i]와 dwarf[j]를 제외하기 위한 if문
                         if(k != i && k != j) System.out.println(dwarf[k]);
                     }
                     return; // return이 존재하지 않으면 result == 100일 때 다시 출력되므로 error
