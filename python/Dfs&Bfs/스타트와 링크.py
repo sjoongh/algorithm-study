d94f515f8592842d994ae86f916124323700aa0a
sjoongh <az45687@naver.com>
2022-05-08 19:40:26 +0900

    fix

--- python/Dfs&Bfs/스타트와 링크.py
+++ python/Dfs&Bfs/스타트와 링크.py
@@ -1,28 +1,36 @@
-def dfs(depth, idx):
-    global min_diff
-    if depth == n//2:
-        power1, power2 = 0, 0
-        for i in range(n):
-            for j in range(n):
-                if visited[i] and visited[j]:
-                    power1 += graph[i][j]
-                elif not visited[i] and not visited[j]:
-                    power2 += graph[i][j]
-        min_diff = min(min_diff, abs(power1-power2))
-        return
+from sys import stdin
+def cal_diff(team_list: list) -> None:
+    another_team_list = list(set(range(N)) - set(team_list))
+    team_sum = 0
+    for i, v in enumerate(team_list[:-1]):
+        for v2 in team_list[i+1:]:
+            team_sum += ability[v][v2] + ability[v2][v]
+    
+    another_team_sum = 0
+    for i, v in enumerate(another_team_list[:-1]):
+        for v2 in another_team_list[i+1:]:
+            another_team_sum += ability[v][v2] + ability[v2][v]
+    
+    ans.append(abs(team_sum - another_team_sum))
 
-    for i in range(idx, n):
-        if not visited[i]:
-            visited[i] = True
-            dfs(depth+1, i+1)
-            visited[i] = False
+    return None
 
 
-n = int(input())
+def dfs(team_list: list) -> None:
+    if len(team_list) == N/2:
+        cal_diff(team_list)
+        return None
+    
+    for i in range(team_list[-1] if team_list else 0, N):
+        if team_list and team_list[0] != 0:
+          return None
+        if i not in team_list:
+            team_list.append(i)
+            dfs(team_list)
+            team_list.pop()
 
-visited = [False for _ in range(n)]
-graph = [list(map(int, input().split())) for _ in range(n)]
-min_diff = int(1e9)
-
-dfs(0, 0)
-print(min_diff)
+N = int(stdin.readline())
+ability = [list(map(int, stdin.readline().split())) for _ in range(N)]
+ans = []
+dfs([])
+print(min(ans))
