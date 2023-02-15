199648d3ad2cb406b8d9d3de1aee26c23d01663f
sjoongh <az45687@naver.com>
2023-02-16 00:18:25 +0900

    [DP 가장 긴 바이토닉 부분 수열]

---
+++ newStart/11054.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+nums = list(map(int, input().split()))
+
+increment = [0 for i in range(n)]
+decrement = [0 for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if (increment[i] >)
