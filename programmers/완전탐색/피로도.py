c223c49cd68f77a4ca44dc16532b90382070abc7
sjoongh <az45687@naver.com>
2023-09-21 17:52:54 +0900

    완전탐색 - [피로도]

--- programmers/완전탐색/피로도.py
+++ programmers/완전탐색/피로도.py
@@ -35,11 +35,22 @@
 # 남은 피로도는 60이며, 세 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 30이므로, 세 번째 던전을 탐험할 수 있습니다. 세 번째 던전의 "소모 피로도"는 10이므로, 던전을 탐험한 후 남은 피로도는 50입니다.
 # 남은 피로도는 50이며, 두 번째 던전을 돌기위해 필요한 "최소 필요 피로도"는 50이므로, 두 번째 던전을 탐험할 수 있습니다. 두 번째 던전의 "소모 피로도"는 40이므로, 던전을 탐험한 후 남은 피로도는 10입니다.
 # 따라서 이 경우 세 던전을 모두 탐험할 수 있으며, 유저가 탐험할 수 있는 최대 던전 수는 3입니다.
+# ---------------------------------------
+import itertools
 
 def solution(k, dungeons):
     answer = []
+    for i in itertools.permutations(dungeons, len(dungeons)):
+        energy = k
+        count = 0
+        for j in i:
+            if j[0] <= energy:
+                count += 1
+                energy -= j[1]
+            else:
+                break
+        answer.append(count)
+    return max(answer)
 
-    return 0
-
-print(solution(80))
+print(solution(80, [[80,20],[50,40],[30,10]]))
 
