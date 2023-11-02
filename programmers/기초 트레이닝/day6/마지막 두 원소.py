ed752a4809b13044c13d4c3ac7bd9c3922eb4e12
sjoongh <az45687@naver.com>
2023-11-02 22:24:53 +0900

    코딩 기초 트레이닝 - Day6(Half)

---
+++ programmers/기초 트레이닝/day6/마지막 두 원소.py
@@ -0,0 +1,8 @@
+# 정수 리스트 num_list가 주어질 때, 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
+
+def solution(num_list):
+    if num_list[-1] > num_list[-2]:
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(num_list[-1]*2)
+    return num_list
